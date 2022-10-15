86392bd308e3b7f207cb088a2d529d518d0daf47
xinxing <2216252906@qq.com>
2022-10-15 15:23:06 +0800

    Initial commit

---
+++ src/main/java/com/example/demogitopt/DemoGitoptApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogitopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitoptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitoptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demogitopt/DemoGitoptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogitopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitoptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
